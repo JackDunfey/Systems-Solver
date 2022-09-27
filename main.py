b166d94611fb80f557ce884dc2a814ddbd1b1ea3
Jack <62919016+pyjshtml@users.noreply.github.com>
2022-09-27 01:18:49 -0400

    update

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import sys
 
 ALPHA = "abcdefghijklmnopqrstuvwxyz"
 toFixed = lambda n,f: round(n * 10**f) / 10**f
-def getLetterFromString(s):
-    for c in s:
-        if c in ALPHA:
-            return c
-    return "_"
+getLetterFromString = lambda s: next((c for c in s if c in ALPHA), "_")
 isolate_terms = lambda equation: list(filter(lambda x: x != "", split(r"[\+=]", sub("-", "+-", sub(r"\s+", "", equation)))))
 def toDictionary(terms_list):
     d = {}
@@ -17,15 +13,6 @@ def toDictionary(terms_list):
         co, var = int(s.group(1)), s.group(2)
         d[var] = co
     return d
-processEquations = lambda equations: map(toDictionary, map(isolate_terms, equations))
-def countTerms(equations):
-    s = set(k for d in equations for k in d)
-    return len(s) - 1 if None in s else 0
-def makeMatrices(equations_dict):
-    v = [[k] for k in list(equations_dict[0])[:-1]]
-    c = [[d[k] for k in [e[0] for e in v]] for d in equations_dict]
-    a = [[d[k] for k in list(d)[-1:]] for d in equations_dict]
-    return c, v, a
 
 def main():
     # Acquire equations
@@ -35,10 +22,13 @@ def main():
     if equations is None:
         raise Exception("input file is empty")
     # Process equations
-    x = list(processEquations(equations))
-    if countTerms(x) > len(x):
+    x = list(map(toDictionary, map(isolate_terms, equations)))
+    s = set(k for d in x for k in d)
+    if (len(s) - 1 if None in s else 0) > len(x):
         raise Exception("too many terms, too few equations")
-    c, v, a = makeMatrices(x)
+    v = [[k] for k in list(x[0])[:-1]]
+    c = [[d[k] for k in [e[0] for e in v]] for d in x]
+    a = [[d[k] for k in list(d)[-1:]] for d in x]
     # Solve equations
     # out = c^-1 * a
     out = [e[0] for e in np.matmul(np.linalg.inv(c),a)]
